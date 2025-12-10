2c17266651afb58dfec275c6d4878c47c17b7ca5
James <198509687+elfinAbjurant@users.noreply.github.com>
2025-12-09 22:16:26 -0600

    Create ui.lua with UI settings for Neovim
    Add general UI options for Neovim configuration

---
+++ dotfiles/nvim/ui.lua
@@ -0,0 +1,15 @@
+-- General UI options to match your Alacritty/Hyprland aesthetic
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.signcolumn = "yes"
+vim.o.scrolloff = 4
+
+-- Indentation / whitespace
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+
+-- Use the same font as Alacritty in GUIs (ignored in terminal)
+vim.g.guifont = "JetBrainsMono Nerd Font:h11"
